efe8d7b5d9da9a417a59fd7913b373a6f841e695
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-08-01 19:03:41 -0300

    requisito 5 completo

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,11 +3,49 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+    };
+  }
+
+  enabledButton = ({ target }) => {
+    const { value } = target;
+    if (value.length > 1) {
+      this.setState({
+        isDisabled: false,
+      });
+    } else {
+      this.setState({
+        isDisabled: true,
+      });
+    }
+  }
+
   render() {
+    const { isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <input
+            type="text"
+            name="pesquisarBanda"
+            placeholder="Nome do Artista"
+            data-testid="search-artist-input"
+            onChange={ this.enabledButton }
+          />
+          <button
+            type="button"
+            disabled={ isDisabled }
+            data-testid="search-artist-button"
+            onClick={ this.pesquisar }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
